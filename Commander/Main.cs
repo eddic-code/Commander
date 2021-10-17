84f245f1335b312cd1cbfe3cf2ac1c1e95464232
eddic.code <eddic.code@gmail.com>
2021-10-16 21:32:15 -0600

    Project references setup.

---
+++ Commander/Main.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityModManagerNet;
+
+namespace Commander
+{
+    public static class Main 
+    {
+        public static bool Enabled;
+
+        public static bool Load(UnityModManager.ModEntry modEntry) 
+        {
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+            return true;
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) 
+        {
+            Enabled = value;
+            return true;
+        }
+    }
+}
