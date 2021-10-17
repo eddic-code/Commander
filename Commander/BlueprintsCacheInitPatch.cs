012f436275e53827401ee1f2fc15fb4d1996b32f
eddic.code <eddic.code@gmail.com>
2021-10-16 21:39:48 -0600

    Added blueprints patch.

---
+++ Commander/BlueprintsCacheInitPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints.JsonSystem;
+
+namespace Commander
+{
+    [HarmonyPatch(typeof(BlueprintsCache), "Init")]
+    internal static class BlueprintsCacheInitPatch
+    {
+        private static bool _loaded;
+
+        internal static void Postfix()
+        {
+            if (_loaded) { return; }
+            _loaded = true;
+
+            // Change blueprints here.
+        }
+    }
+}
