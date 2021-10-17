2a139a3a2141d5a8466dfdcf44283507b4be763c
Eddic-Code <eddic.code@gmail.com>
2021-10-17 16:09:10 -0600

    Added saint miracle spells.

--- Commander/Archetypes/DivineSaintArchetype.cs
+++ Commander/Archetypes/DivineSaintArchetype.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Commander.Components;
 using HarmonyLib;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Selection;
+using Kingmaker.Blueprints.Items.Armors;
 using Kingmaker.Designers.Mechanics.Facts;
 using Kingmaker.EntitySystem.Stats;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
 using Kingmaker.UnitLogic.FactLogic;
 
 namespace Commander.Archetypes
@@ -14,6 +18,134 @@ namespace Commander.Archetypes
     {
         public static void Create()
         {
+            var oracle = Resources.GetBlueprint<BlueprintCharacterClass>("20ce9bf8af32bee4c8557a045ab499b1");
+            var lifeFinalRevelation = Resources.GetBlueprint<BlueprintFeature>("ee23b52c6a06c0b48a09a7a23071aa52");
+            var mysterySelection = Resources.GetBlueprint<BlueprintFeatureSelection>("5531b975dcdf0e24c98f1ff7e017e741");
+
+            // Saint Mystery
+            // 1 - Remove Sickness
+            var removeSickness = Resources.GetBlueprint<BlueprintAbility>("f6f95242abdfac346befd6f4f6222140").ToReference<BlueprintAbilityReference>();
+            var barkskin = Resources.GetBlueprint<BlueprintAbility>("5b77d7cc65b8ab74688e74a37fc2f553").ToReference<BlueprintAbilityReference>();
+            var heroism = Resources.GetBlueprint<BlueprintAbility>("5ab0d42fb68c9e34abae4921822b9d63").ToReference<BlueprintAbilityReference>();
+            var restoration = Resources.GetBlueprint<BlueprintAbility>("f2115ac1148256b4ba20788f7e966830").ToReference<BlueprintAbilityReference>();
+            var breadthOfLife = Resources.GetBlueprint<BlueprintAbility>("d5847cad0b0e54c4d82d6c59a3cda6b0").ToReference<BlueprintAbilityReference>();
+            var heal = Resources.GetBlueprint<BlueprintAbility>("5da172c4c89f9eb4cbb614f3a67357d3").ToReference<BlueprintAbilityReference>();
+            var restorationGreater = Resources.GetBlueprint<BlueprintAbility>("fafd77c6bfa85c04ba31fdc1c962c914").ToReference<BlueprintAbilityReference>();
+            var healMass = Resources.GetBlueprint<BlueprintAbility>("867524328b54f25488d371214eea0d90").ToReference<BlueprintAbilityReference>();
+            var heroicInvocation = Resources.GetBlueprint<BlueprintAbility>("43740dab07286fe4aa00a6ee104ce7c1").ToReference<BlueprintAbilityReference>();
+            
+            var spell1 = Helpers.Create<AddKnownSpell>(n =>
+            {
+                n.SpellLevel = 1;
+                n.m_CharacterClass = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Spell = removeSickness;
+            });
+
+            // 2 - Barkskin
+            var spell2 = Helpers.Create<AddKnownSpell>(n =>
+            {
+                n.SpellLevel = 2;
+                n.m_CharacterClass = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Spell = barkskin;
+            });
+
+            // 3 - Heroism
+            var spell3 = Helpers.Create<AddKnownSpell>(n =>
+            {
+                n.SpellLevel = 3;
+                n.m_CharacterClass = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Spell = heroism;
+            });
+
+            // 4 - Restoration
+            var spell4 = Helpers.Create<AddKnownSpell>(n =>
+            {
+                n.SpellLevel = 4;
+                n.m_CharacterClass = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Spell = restoration;
+            });
+
+            // 5 - Breadth of Life
+            var spell5 = Helpers.Create<AddKnownSpell>(n =>
+            {
+                n.SpellLevel = 5;
+                n.m_CharacterClass = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Spell = breadthOfLife;
+            });
+
+            // 6 - Heal
+            var spell6 = Helpers.Create<AddKnownSpell>(n =>
+            {
+                n.SpellLevel = 6;
+                n.m_CharacterClass = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Spell = heal;
+            });
+
+            // 7 - Restoration, Greater
+            var spell7 = Helpers.Create<AddKnownSpell>(n =>
+            {
+                n.SpellLevel = 7;
+                n.m_CharacterClass = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Spell = restorationGreater;
+            });
+
+            // 8 - Heal, Mass
+            var spell8 = Helpers.Create<AddKnownSpell>(n =>
+            {
+                n.SpellLevel = 8;
+                n.m_CharacterClass = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Spell = healMass;
+            });
+
+            // 9 - Heroic Invocation
+            var spell9 = Helpers.Create<AddKnownSpell>(n =>
+            {
+                n.SpellLevel = 9;
+                n.m_CharacterClass = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Spell = heroicInvocation;
+            });
+
+            var spellList = Helpers.Create<AddSpellsToDescription>(n =>
+            {
+                n.m_Spells = new[]
+                {
+                    removeSickness, barkskin, heroism, restoration, breadthOfLife, heal, restorationGreater, healMass,
+                    heroicInvocation
+                };
+            });
+
+            var saintMysterySpells = Helpers.CreateBlueprint<BlueprintFeature>("SaintMysterySpells", "9376ccdc3150488ab7a92ba5c377bd4c", n =>
+            {
+                n.IsClassFeature = true;
+                n.HideInCharacterSheetAndLevelUp = true;
+                n.Ranks = 1;
+                n.AddComponents(spell1, spell2, spell3, spell4, spell5, spell6, spell7, spell8, spell9, spellList);
+            });
+
+            var saintMysterySpellsComp = Helpers.Create<AddFeatureOnClassLevel>(n =>
+            {
+                n.Level = 2;
+                n.m_Class = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Feature = saintMysterySpells.ToReference<BlueprintFeatureReference>();
+            });
+
+            var finalRevelationComp = Helpers.Create<AddFeatureOnClassLevel>(n =>
+            {
+                n.Level = 20;
+                n.m_Class = oracle.ToReference<BlueprintCharacterClassReference>();
+                n.m_Feature = lifeFinalRevelation.ToReference<BlueprintFeatureReference>();
+            });
+
+            var saintMystery = Helpers.CreateBlueprint<BlueprintFeature>("SaintMystery", "37457aa3c5dc4791a544c66ea832c3b5", n =>
+            {
+                n.SetName("Saint Mystery");
+                n.SetDescription("Oracles who walk the saintly path.");
+                n.Groups = new[] {FeatureGroup.OracleMystery};
+                n.IsClassFeature = true;
+                n.Ranks = 1;
+                n.AddComponents(finalRevelationComp, saintMysterySpellsComp);
+            });
+
             // Saint's Intuition
             var replaceAcComp = Helpers.Create<ReplaceStatBaseAttribute>(n =>
             {
@@ -38,13 +170,19 @@ namespace Commander.Archetypes
                 n.NewStat = StatType.Charisma;
             });
 
+            var ignorePenaltiesComp = Helpers.Create<IgnoreArmorPenaltiesComp>(n =>
+            {
+                n.Categories = new HashSet<ArmorProficiencyGroup>{ArmorProficiencyGroup.Light, ArmorProficiencyGroup.Medium};
+            });
+
             var saintsIntuition = Helpers.CreateBlueprint<BlueprintFeature>("SaintsIntuition", "5d4ff3da5da6401aba8a068d1aa11ad3", n =>
             {
                 n.SetName("Saint's Intuition");
-                n.SetDescription("Use your charisma as a bonus for AC, perception, and religion.");
+                n.SetDescription("You may add your Charisma modifier, instead of your Dexterity modifier, to your Armor Class and CMD. The maximum Dexterity bonus to AC does not apply to you when wearing light armor, medium armor, or wearing a shield. You may also use your Charisma bonus for Perception and Lore Religion checks.");
                 n.IsClassFeature = true;
                 n.Ranks = 1;
-                n.AddComponents(replaceAcComp, replacePerceptionComp, replaceReligionComp, replaceCmdComp);
+                n.ReapplyOnLevelUp = true;
+                n.AddComponents(replaceAcComp, replacePerceptionComp, replaceReligionComp, replaceCmdComp, ignorePenaltiesComp);
             });
 
             // Saint's Presence
@@ -61,12 +199,11 @@ namespace Commander.Archetypes
                 n.SetDescription("While wearing medium or light armor, the divine saint can add their charisma bonus to their AC.");
                 n.IsClassFeature = true;
                 n.Ranks = 1;
+                n.ReapplyOnLevelUp = true;
                 n.AddComponents(saintsPresenceComp, recalculateComp);
             });
 
             // Archetype creation.
-            var oracle = Resources.GetBlueprint<BlueprintCharacterClass>("20ce9bf8af32bee4c8557a045ab499b1");
-
             var archetype = Helpers.Create<BlueprintArchetype>(a =>
             {
                 a.name = "DivineSaintArchetype";
@@ -77,7 +214,8 @@ namespace Commander.Archetypes
 
             archetype.ReplaceClassSkills = true;
             archetype.ClassSkills = new[] { StatType.SkillLoreReligion, StatType.SkillPerception, StatType.SkillPersuasion, StatType.SkillUseMagicDevice };
-            archetype.AddFeatures = new[] { Helpers.LevelEntry(1, saintsPresence), Helpers.LevelEntry(1, saintsIntuition) };
+            archetype.AddFeatures = new[] { Helpers.LevelEntry(1, saintsPresence), Helpers.LevelEntry(1, saintsIntuition), Helpers.LevelEntry(1, saintMystery)};
+            archetype.RemoveFeatures = new[] { Helpers.LevelEntry(1, mysterySelection)};
 
             oracle.m_Archetypes = oracle.m_Archetypes.AddToArray(archetype.ToReference<BlueprintArchetypeReference>()).ToArray();
             Resources.AddBlueprint(archetype);
---
+++ Commander/Components/IgnoreArmorPenaltiesComp.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Kingmaker.Blueprints.Items.Armors;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic;
+
+namespace Commander.Components
+{
+    [TypeId("b55d5f359b294f14ad2a74856b6c6c58")]
+    public class IgnoreArmorPenaltiesComp : UnitFactComponentDelegate, IInitiatorRulebookHandler<RuleCalculateArmorMaxDexBonusLimit>
+    {
+        public HashSet<ArmorProficiencyGroup> Categories;
+
+        public override void OnTurnOn() 
+        {
+            base.OnTurnOn();
+
+            if (Owner.Body.Armor.HasArmor && Owner.Body.Armor.Armor.Blueprint.IsArmor) 
+            {
+                Owner.Body.Armor.Armor.RecalculateStats();
+                Owner.Body.Armor.Armor.RecalculateMaxDexBonus();
+            }
+
+            if (Owner.Body.SecondaryHand.HasShield && Owner.Body.SecondaryHand.MaybeShield != null) 
+            {
+                Owner.Body.SecondaryHand.MaybeShield.ArmorComponent.RecalculateStats();
+                Owner.Body.SecondaryHand.MaybeShield.ArmorComponent.RecalculateMaxDexBonus();
+            }
+        }
+
+        public void OnEventAboutToTrigger(RuleCalculateArmorMaxDexBonusLimit evt)
+        {
+            
+        }
+
+        public void OnEventDidTrigger(RuleCalculateArmorMaxDexBonusLimit evt)
+        {
+            if (!evt.Armor.Blueprint.IsShield && Categories.Contains(evt.Armor.ArmorType())) 
+            {
+                evt.Result = null;
+                return;
+            }
+
+            if (evt.Armor.Blueprint.IsShield) 
+            {
+                evt.Result = null;
+            }
+        }
+    }
+}
