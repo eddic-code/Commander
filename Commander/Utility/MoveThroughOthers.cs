937cd849d289cf16cbb10a70647206e34c1d67be
Eddic-Code <eddic.code@gmail.com>
2021-10-18 13:41:37 -0600

    Added utility.

---
+++ Commander/Utility/MoveThroughOthers.cs
@@ -0,0 +1,40 @@
+﻿// ReSharper disable InconsistentNaming
+
+using HarmonyLib;
+using Kingmaker.View;
+
+namespace Commander.Utility
+{
+    public static class MoveThroughOthers
+    {
+        [HarmonyPatch(typeof(UnitMovementAgent), nameof(UnitMovementAgent.AvoidanceDisabled), MethodType.Getter)]
+        internal static class UnitMovementAgentAvoidanceDisabledPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(UnitMovementAgent __instance, ref bool __result)
+            {
+                var entityData = __instance.Unit?.EntityData;
+
+                if (entityData != null && entityData.IsPlayerFaction) 
+                {
+                    __result = true;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(UnitMovementAgent), "IsSoftObstacle", typeof(UnitMovementAgent))]
+        internal static class UnitMovementAgent_IsSoftObstacle_Patch 
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(UnitMovementAgent __instance, ref bool __result) 
+            {
+                var entityData = __instance.Unit?.EntityData;
+                if (entityData != null && entityData.IsPlayerFaction) { return true; }
+
+                __result = !__instance.CombatMode;
+
+                return false;
+            }
+        }
+    }
+}
---
+++ Commander/Utility/MultiplierTweaks.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable InconsistentNaming
+
+using HarmonyLib;
+using Kingmaker.UnitLogic;
+using UnityEngine;
+
+namespace Commander.Utility
+{
+    public static class MultiplierTweaks
+    {
+        [HarmonyPatch(typeof(EncumbranceHelper), "GetHeavy")]
+        internal static class EncumbranceHelperGetHeavyPatch 
+        {
+            internal static void Postfix(ref int __result) 
+            {
+                __result = Mathf.RoundToInt(__result * 4);
+            }
+        }
+    }
+}
