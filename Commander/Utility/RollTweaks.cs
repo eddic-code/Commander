35fc682cb66e292c4760d1d6bcc968f25d41f9d2
Eddic-Code <eddic.code@gmail.com>
2021-10-25 23:29:49 -0600

    Added object highlight.

---
+++ Commander/Utility/RollTweaks.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable InconsistentNaming
+
+using System;
+using HarmonyLib;
+using Kingmaker.RuleSystem;
+using Kingmaker.RuleSystem.Rules;
+
+namespace Commander.Utility
+{
+    [HarmonyPatch(typeof(RuleRollDice), "Roll")]
+    public static class RollTweaks
+    {
+        private static void Postfix(RuleRollDice __instance)
+        {
+            if (__instance.DiceFormula.Dice != DiceType.D20) { return; }
+
+            var initiator = __instance.Initiator;
+            if (!initiator.IsPlayerFaction || initiator.IsInCombat) { return; }
+
+            __instance.m_Result = Math.Max(10, __instance.Result);
+        }
+    }
+}
