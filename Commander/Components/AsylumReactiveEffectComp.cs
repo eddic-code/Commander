bcff1c07a7f3a21e9429ac33e56130c69407127e
Eddic-Code <eddic.code@gmail.com>
2021-10-27 22:19:01 -0600

    Working on Asylum.

---
+++ Commander/Components/AsylumReactiveEffectComp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Facts;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules.Damage;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+
+namespace Commander.Components
+{
+    [AllowedOn(typeof(BlueprintUnitFact))]
+    [TypeId(Guids.AsylumReactiveEffectComp)]
+    public class AsylumReactiveEffectComp : UnitFactComponentDelegate, ITargetRulebookHandler<RuleDealDamage>
+    {
+        private static BlueprintBuff _defensiveBuff;
+
+        private static BlueprintBuff DefensiveBuff
+        {
+            get
+            {
+                return _defensiveBuff ??=
+                    ResourcesLibrary.TryGetBlueprint(new BlueprintGuid(Guid.Parse(Guids.AsylumDefensiveBuff))) as
+                        BlueprintBuff;
+            }
+        }
+
+        public void OnEventAboutToTrigger(RuleDealDamage evt)
+        {
+            
+        }
+
+        public void OnEventDidTrigger(RuleDealDamage evt)
+        {
+            if (evt.Result <= 0 || Owner.State.IsDead || Owner.State.IsUnconscious || !Owner.CombatState.IsInCombat) { return; }
+
+            var buff = Owner.Buffs.GetBuff(DefensiveBuff);
+            if (buff != null && buff.TimeLeft > TimeSpan.Zero) { return; }
+
+            if (Owner.HPLeft > Owner.MaxHP * 0.5f) { return; }
+
+            Owner.AddBuff(DefensiveBuff, Context);
+        }
+    }
+}
---
+++ Commander/Guids.cs
@@ -0,0 +1,8 @@
+﻿namespace Commander
+{
+    internal static class Guids
+    {
+        public const string AsylumDefensiveBuff = "c12c2461f6234060986046aa0c569593";
+        public const string AsylumReactiveEffectComp = "0392aff018214a19ba042aca0008f959";
+    }
+}
